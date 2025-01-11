7b28f87f86aa3664db30872ed6dbf4ea1185577a
rina2525 <rina.yasuno@icloud.com>
2025-01-12 00:04:40 +0900

    Create KatoTaro_Chapter18.java

---
+++ kadai_018/KatoTaro_Chapter18.java
@@ -0,0 +1,20 @@
+package kadai_018;
+	public class KatoTaro_Chapter18 extends Kato_Chapter18{
+		
+		public KatoTaro_Chapter18() {
+			setGivenName();
+		}
+		
+		public void setGivenName() {
+			givenName = "太郎";
+		
+		
+	}
+	
+	//抽象メソッド実行
+		public void eachIntroduce() {
+			System.out.println("私はJavaが得意です");
+		}
+	
+	}
+
