f1f168bd5ccc02064397fd9b91a497d85e72b2c4
rina2525 <rina.yasuno@icloud.com>
2025-01-14 23:48:06 +0900

    Update KatoExec_Chapter18.java

--- kadai_018/KatoExec_Chapter18.java
+++ kadai_018/KatoExec_Chapter18.java
@@ -4,18 +4,22 @@ public class KatoExec_Chapter18 {
 
 	public static void main(String[] args) {
 		//インスタンスを作成
-		Kato_Chapter18 taro = new KatoTaro_Chapter18();
-		
+		KatoTaro_Chapter18 taro = new KatoTaro_Chapter18();
+		taro.setGivenName();
 		taro.execIntroduce();
 		System.out.println();
 		
-		Kato_Chapter18 ichiro = new KatoIchiro_Chapter18();
+		KatoIchiro_Chapter18 ichiro = new KatoIchiro_Chapter18();
+		ichiro.setGivenName();
 		ichiro.execIntroduce();
 		System.out.println();
 		
 		
-		Kato_Chapter18 hanako =new KatoHanako_Chapter18();
+		KatoHanako_Chapter18 hanako =new KatoHanako_Chapter18();
+		hanako.setGivenName();
 		hanako.execIntroduce();
-	}
+	
+
+}
 
 }
